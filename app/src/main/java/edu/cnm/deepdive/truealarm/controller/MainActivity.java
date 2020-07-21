a4a1628c67893e958aadb85d7344f0c192b2b30b
Paul C <pcutter@cnm.edu>
2020-07-21 09:52:50 -0600

    Reformatted detail view - added est commute time
    Need to fix clutter. Arrival Time Title not showing up.

--- app/src/main/java/edu/cnm/deepdive/truealarm/controller/MainActivity.java
+++ app/src/main/java/edu/cnm/deepdive/truealarm/controller/MainActivity.java
@@ -1,7 +1,6 @@
 package edu.cnm.deepdive.truealarm.controller;
 
 import android.os.Bundle;
-import android.widget.TimePicker;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
@@ -20,7 +19,7 @@ public class MainActivity extends AppCompatActivity {
     // Passing each menu ID as a set of Ids because each
     // menu should be considered as top level destinations.
     AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-        R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
+        R.id.navigation_home, R.id.navigation_alarm_detail, R.id.navigation_settings)
         .build();
     NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
     NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
