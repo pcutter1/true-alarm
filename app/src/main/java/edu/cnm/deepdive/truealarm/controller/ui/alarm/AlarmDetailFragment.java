aeeed2d760bd4ec41800c126031dbdd119dd475c
Paul C <pcutter@cnm.edu>
2020-07-25 13:21:50 -0600

    Setup for google maps - began implementation

--- app/src/main/java/edu/cnm/deepdive/truealarm/controller/ui/alarm/AlarmDetailFragment.java
+++ app/src/main/java/edu/cnm/deepdive/truealarm/controller/ui/alarm/AlarmDetailFragment.java
@@ -1,5 +1,7 @@
 package edu.cnm.deepdive.truealarm.controller.ui.alarm;
 
+import static edu.cnm.deepdive.truealarm.util.Constants.MAPVIEW_BUNDLE_KEY;
+
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,25 +12,102 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import edu.cnm.deepdive.truealarm.R;
 import edu.cnm.deepdive.truealarm.viewmodel.AlarmDetailViewModel;
 
-public class AlarmDetailFragment extends Fragment {
+public class AlarmDetailFragment extends Fragment implements OnMapReadyCallback {
 
   private AlarmDetailViewModel alarmDetailViewModel;
+  private MapView mMapView;
+
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+
+    // *** IMPORTANT ***
+    // MapView requires that the Bundle you pass contain _ONLY_ MapView SDK
+    // objects or sub-Bundles.
+    Bundle mapViewBundle = null;
+    if (savedInstanceState != null) {
+      mapViewBundle = savedInstanceState.getBundle(MAPVIEW_BUNDLE_KEY);
+    }
+    mMapView.onCreate(mapViewBundle);
+
+    mMapView.getMapAsync(this);
+  }
 
   public View onCreateView(@NonNull LayoutInflater inflater,
       ViewGroup container, Bundle savedInstanceState) {
-    alarmDetailViewModel =
-        ViewModelProviders.of(this).get(AlarmDetailViewModel.class);
-    View root = inflater.inflate(R.layout.fragment_alarm_detail, container, false);
-    final TextView textView = root.findViewById(R.id.select_arrival_time_button);
-    alarmDetailViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-      @Override
-      public void onChanged(@Nullable String s) {
-        textView.setText(s);
-      }
-    });
-    return root;
+    View view = inflater.inflate(R.layout.fragment_alarm_detail, container, false);
+    mMapView = view.findViewById(R.id.route_map);
+    final TextView textView = view.findViewById(R.id.select_arrival_time_button);
+//    alarmDetailViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+//      @Override
+//      public void onChanged(@Nullable String s) {
+//        textView.setText(s);
+//      }
+//    });
+    return view;
   }
+
+
+  @Override
+  public void onSaveInstanceState(Bundle outState) {
+    super.onSaveInstanceState(outState);
+
+    Bundle mapViewBundle = outState.getBundle(MAPVIEW_BUNDLE_KEY);
+    if (mapViewBundle == null) {
+      mapViewBundle = new Bundle();
+      outState.putBundle(MAPVIEW_BUNDLE_KEY, mapViewBundle);
+    }
+
+    mMapView.onSaveInstanceState(mapViewBundle);
+  }
+
+  @Override
+  public void onResume() {
+    super.onResume();
+    mMapView.onResume();
+  }
+
+  @Override
+  public void onStart() {
+    super.onStart();
+    mMapView.onStart();
+  }
+
+  @Override
+  public void onStop() {
+    super.onStop();
+    mMapView.onStop();
+  }
+
+  @Override
+  public void onMapReady(GoogleMap map) {
+    map.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
+  }
+
+  @Override
+  public void onPause() {
+    mMapView.onPause();
+    super.onPause();
+  }
+
+  @Override
+  public void onDestroy() {
+    mMapView.onDestroy();
+    super.onDestroy();
+  }
+
+  @Override
+  public void onLowMemory() {
+    super.onLowMemory();
+    mMapView.onLowMemory();
+  }
+
 }
---
+++ app/src/main/java/edu/cnm/deepdive/truealarm/util/Constants.java
@@ -0,0 +1,8 @@
+package edu.cnm.deepdive.truealarm.util;
+
+public class Constants {
+
+  public static final String MAPVIEW_BUNDLE_KEY = "MapViewBundleKey";
+
+
+}
